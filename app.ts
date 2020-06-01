30a7fb4dec52a5db51f9f11306304d1dca74d9d2
drmeloy <drmeloy@gmail.com>
2020-06-01 10:21:29 -0700

    added update dog function

--- app.ts
+++ app.ts
@@ -56,13 +56,38 @@ const addDog = async ({
   response.status = 200;
 }
 
+const updateDog = async ({
+  params,
+  request,
+  response
+}: {
+  params: {
+    name: string
+  },
+  request: any,
+  response: any
+}) => {
+  const temp = dogs.filter(existingDog => existingDog.name === params.name);
+  const body = await request.body();
+  const { age }: { age: number } = body.value;
+
+  if (temp.length) {
+    temp[0].age = age;
+    response.status = 200;
+    response.body = { msg: 'OK' };
+    return;
+  }
+  response.status = 400;
+  response.body = { msg: `Cannot find dog ${params.name}` };
+}
+
 const router = new Router();
 
 router
   .get('/dogs', getDogs)
   .get('/dogs/:name', getDog)
   .post('/dogs', addDog)
-  // .put('/dogs/:name', updateDog)
+  .put('/dogs/:name', updateDog)
   // .delete('/dogs/:name', removeDog)
 
 
